0cc5b5bf20f072e50ac9d1ac6f05ecb913d19e30
bigbl <adamkostandy@gmail.com>
2023-03-10 09:28:40 -0500

    Created useBookData hook and implemented

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,43 +1,74 @@
 import Container from '../components/Container';
-import {useParams} from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 import ErrorAlert from "../components/ErrorAlert.jsx";
 import Skeleton from "react-loading-skeleton";
 import {useEffect, useState} from "react";
+import useBookData from "../hooks/useBookData.js";
+
 
 const Book = () => {
 
     const params = useParams();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-    const [bookData, setBookData] = useState(null);
-    const getData = async () => {
-        const url = `https://api.matgargano.com/api/books/${params.id}`;
-        console.log(url);
-        setLoading(true);
-        setError(false);
-        try {
-            const request = await fetch(url);
-            const response = await request.json();
-            setBookData(response);
-        } catch(e) {
-            setError('Error: ' + e.message);
-        } finally {
-            setLoading(false);
-        }
-    }
 
-    useEffect(() => {
-        getData();
-    }, []);
-
-
+    const [loading, error, bookData] = useBookData(params.id);
 
     return (<Container>
         {error && <ErrorAlert>{error}</ErrorAlert>}
             {!error && loading && <div className="max-w-[230px]"><Skeleton count="5" /></div>}
             {!error && !loading &&
                 <>
-                    <div className= "grid min-w-full bg-amber-100 grid-cols-3 grid-rows-3 border solid border-4 " > {bookData.id} </div>
+                    <section className= "grid min-w-full border solid border-4 bg-gradient-to-r from-rose-100 to-teal-100 " >
+
+
+                        <Link className=" m-2 absolute inline-block rounded-full border border-indigo-600 bg-white p-3 text-indigo-500 hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+                           to="/books"
+                        >
+                            <span className="sr-only"> Download </span>
+
+                            <svg
+                                className="h-5 w-5 rotate-180"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    d="M14 5l7 7m0 0l-7 7m7-7H3"
+                                />
+                            </svg>
+                        </Link>
+
+
+                        <div className="  text-center  ">
+                            <h2 className= " text-5xl p-5 font-black"> {!bookData ? "Loading" : bookData.title} </h2>
+                            <h3 className= " text-lg p-5 font-bold"> {!bookData ? "Loading" : ` ✍️ Written by: ${bookData.author} 📚 Published by: ${bookData.publisher}`} </h3>
+                        </div>
+
+                        <div>
+                            <img  alt = {!bookData ? "" : bookData.imageURL} className=" my-4 mx-auto " src={!bookData ? "" : bookData.imageURL}/>
+                        </div>
+
+                        <div>
+
+                            <div className="flex justify-center">
+                                <ul className="w-96 justify-self-center">
+                                    <li
+                                        className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4 dark:border-opacity-50">
+                                         Year Released: {!bookData ? "" : bookData.year}
+                                    </li>
+                                    <li
+                                        className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4 dark:border-opacity-50">
+                                        Amount of Pages: {!bookData ? "" : bookData.pages}
+                                    </li>
+                                    <li
+                                        className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4 dark:border-opacity-50">
+                                        Created in {!bookData ? "" : bookData.country}
+                                    </li>
+                                </ul>
+                            </div>
+
+                        </div>
+                    </section>
                 </>
             }
     </Container>)
